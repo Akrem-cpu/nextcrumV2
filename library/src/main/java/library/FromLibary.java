bad5fb087a6c135a2fad03f32adaa9136562a364
akrem-cpu <akremabdu71@gmail.com>
2021-11-24 21:44:41 -0600

    init commit

---
+++ library/src/main/java/library/FromLibary.java
@@ -0,0 +1,13 @@
+package library;
+
+public class FromLibary {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ nextcrm-automation/src/test/java/nextcrm/nextCrm/test2.java
@@ -0,0 +1,11 @@
+package nextcrm.nextCrm;
+
+import library.FromLibary;
+
+public class test2 {
+    public static void main(String[] args) {
+        FromLibary fromLibary = new FromLibary();
+        fromLibary.setName("Enum");
+        System.out.println("fromLibary.getName() = " + fromLibary.getName());
+    }
+}
